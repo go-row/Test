0a6689707fb2bd445f93ac6f80442e9c92715b5a
Gaurav Singh <gaksidjax1614@gmail.com>
2020-12-10 23:10:00 +0530

    Add solution for problem `A - Nearly Lucky Number` Link: https://codeforces.com/contest/110/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/110/submission/100856121

---
+++ codeforces/110/A.cpp
@@ -0,0 +1,67 @@
+//gauravsingh :)
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//using int = long long int;
+using ll = long long int;
+using pii = pair < int, int >;
+
+#define ff first
+#define ss second
+#define endl '\n'
+#define pb push_back
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define rep(i, j, n)	for(int i = j; i < n; i++)
+#define ren(i, j, n)	for(int i = n-1; i >= j; i--)
+
+#define cerr cout
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {
+    cerr << t; if(sizeof...(v)) cerr << ", "; _print(v...);
+}
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+
+const int mod = 1000000007;
+
+void io_set(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve(){
+    string s;   cin >> s;
+    int c = 0;
+    for(int i = 0; i < s.size(); i++){
+        c += (s[i] == '4' || s[i] == '7');
+    }
+    bool flag = true;
+    if(c == 0)  flag = false;
+    while(c){
+        int rem = c%10;
+        if(rem == 4 || rem == 7){
+            c /= 10;
+            continue;
+        }
+        else{
+            flag = false;
+            break;
+        }
+    }
+    cout << (flag ? "YES" : "NO") << '\n';
+}
+
+signed main(int argc, char **argv){
+    io_set();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int tt = 1;
+    //cin >> tt;
+    while(tt--){
+        solve();
+    }
+    return 0;
+}
+
